10413ca8520388768ac54c2a19eb4c2fd56cd206
lai2-01 <lai2-01@itismeucci.com>
2021-11-17 11:20:36 +0100

    modifiche finali

--- chat client/src/main/java/it/meucci/AppCLI.java
+++ chat client/src/main/java/it/meucci/AppCLI.java
@@ -7,8 +7,8 @@ public class AppCLI
     public static void main( String[] args ) throws IOException
     {
         client client1 = new client();
-        client1.connetti();
         gui Interfaccia=new gui(client1);
+        client1.connetti();
         client1.Grafica=Interfaccia;
         client1.controllo.Grafica=Interfaccia;
         client1.comunica();
